596001be0f0185d1277b851c09dc51a5ba3c62b7
Gustavo Silva <Gustavo.katia.rodrigues@gmail.com>
2025-10-20 07:27:09 -0400

    feat: creating a new fun() DeleteAddress

--- controllers/address.go
+++ controllers/address.go
@@ -1,6 +1,14 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gustavoz65/e-commerce-back/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func AddAddress() gin.HandlerFunc {
 
@@ -15,5 +23,32 @@ func EditWorkAddress() gin.HandlerFunc {
 }
 
 func DeleteAddress() gin.HandlerFunc {
+      return func (c *gin.Context) {
+		  user_id := c.Query("id")
 
+		  if user_id == "" {
+            c.Header("Contect-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H("ërror": "user id is missing"))
+			c.Abort()
+			return 
+		  }
+		  address := make([]models.Address, 0)
+		  usert_id, err := primitive.ObjectIDFromHex(user_id)
+	      if err != nil {
+			c.IndentedJSON(hhtp.StatusBadResquest, "Internal server error")
+		  }
+		  ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		  defer cancel()
+
+		  filter := bson.D{primitive.E{key: "_id", Value: usert_id}}   // chaves de id de atualização do mongodb
+		  update := bson.D{{key: "$set", Value: bson.D{primitive.E{key: "address", Value: address}}}} // seta o campo de endereço como uma matriz vazia
+		
+		  _, err := UserCollection.UpdateOne(ctx,filter, update)
+		  if err != nil {
+			c.IndentedJSON(http.StatusBadRequest,"message": "wordng command")
+		  }
+		  defer cancel()
+		  ctx.Done()
+		  c.IndentedJSON(http.StatusOK, "success": "address deleted successfully")
+		}
 }
